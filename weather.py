e883d68ea8c247957da1bd750fba228c6b91e339
jwiebe92 <45601015+jwiebe92@users.noreply.github.com>
2019-01-23 09:28:06 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -7,8 +7,11 @@ import pandas as pd
 import glob
 
 
+# TODO StationID?
+# TODO CSV Spaltennamen?
+# TODO Scientific Notation von Messdatum?
+
 # station_ID ist die ID der jeweiligen Stadt aus der .txt
-# Stations ID ist  variabel
 def weather_info(station_name: str):
     list_station_zip = []
     txt_files = []
@@ -24,8 +27,13 @@ def weather_info(station_name: str):
         print("Station nicht vorhanden!")
     else:
         list_station_zip = list(map(lambda x: x + ".zip", list_station))
-        # print(list_station_zip)
 
+    txt_files = unzip_search_txt(list_station_zip, txt_files)
+
+    save_to_csv(txt_files)
+
+
+def unzip_search_txt(list_station_zip, txt_files):
     for zip_data in list_station_zip:
         # FTP server path
         download_path = "ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/10_minutes/air_temperature" \
@@ -41,24 +49,29 @@ def weather_info(station_name: str):
             print('Fertig!')  # Dateien müssten im Ordner extrahiert sein
             # lese extrahierte Dateien mit Endung - .txt
             txt_files = glob.glob("*.txt")
+    return txt_files
 
+
+def save_to_csv(txt_files):
     for txt_data in txt_files:
         # aktueller Pfad
         current_path = os.getcwd()
         # in csv umwandeln/exportieren
         df = pd.read_csv(current_path + "\\" + txt_data + "")
+        print(df.head(5))
+        print(df.shape)
+
         # df.columns = [
         #     'Stations ID , Messdatum , QN , Luftdruck in Stationshoehe der voran. 10 min , momentane Lufttemperatur in 2m Hoehe, Momentane Temperatur in 5 cm Hoehe, relative Feucht. in 2m Hoehe, Taupunkttemperatur in 2m, Taupunkttemperatur in 2m Hoehe, eor']
+
+        # Das das Messdatum falsch formattiert in Excel gespeichert wird, lieg an Excel selbst. -> Spalte muss auf Zahlenformat umgestellt werden
         df.to_csv(txt_data + ".csv", sep=",", encoding='utf-8', index=False)
         # df_csv = pd.read_csv(txt_data + ".csv")
         # df_csv.rename(columns=({'STATIONS_ID': 'Stations ID' , 'MESS_DATUM': 'Messdatum' , 'QN': 'QN' , 'PP_10': 'Luftdruck in Stationshoehe der voran. 10 min' , 'TT_10': 'momentane Lufttemperatur in 2m Hoehe',
         #                     'TM5_10': 'Momentane Temperatur in 5 cm Hoehe', 'RF_10': 'relative Feucht. in 2m Hoehe', 'TD_10': 'Taupunkttemperatur in 2m', 'eor': 'eor'}))
-        #
 
         # TODO Spaltennamen ändern und MessDatum muss korrekt übergeben werden
 
-# TODO vielleicht einfach eine Excel kreieren , Spaltennamen definieren und
-#  pro Line reinschreieben (damit 1:1 das Messdatum übernommen wird)
 
 # -> sollte "Station nicht vorhanden!" ausgeben
 weather_info("00797")
